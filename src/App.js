f9a87c8bbf73dd295447b34b97e355aacedcc212
Beaune <mathis.beaune13@gmail.com>
2021-03-31 09:19:01 +0200

    Counter finit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      FirstName: "John",
+      LastName: this.props.LastName,
+      counter: 0,
+    };
+  }
+
+  increment = () => {
+    this.setState({ counter: this.state.counter +1 });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>My name is {this.state.FirstName} {this.state.LastName} </p>
+          <p> {this.state.counter} </p>
+          <button onClick={this.increment}>Click</button>
+          <button onClick={() => this.setState({ counter: 0 })}>reset</button>
+        </header>
+      </div>
+    )
+  }
 }
+
+
 
 export default App;
